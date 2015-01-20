a87d8924f70c75c81f050573522dc2decf12c380
ksksAsa <skakaes@hotmail.com>
2015-01-20 20:20:21 +0200

    create user admin

--- SimpleBlog/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog/Areas/Admin/Controllers/UsersController.cs
@@ -24,5 +24,101 @@ namespace SimpleBlog.Areas.Admin.Controllers
             });
         }
 
+        public ActionResult New()
+        {
+            return View(new UsersNew { });
+        }
+
+        [HttpPost]
+        public ActionResult New(UsersNew form)
+        {
+            if (Database.Session.Query<User>().Any(u => u.Username == form.Username))
+                ModelState.AddModelError("Username", "Username must be unique");
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            var user = new User
+            {
+                Email = form.Email,
+                Username = form.Username
+            };
+
+            user.SetPassword(form.Password);
+            Database.Session.Save(user);
+
+            return RedirectToAction("index");
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var user = Database.Session.Load<User>(Id);
+            if (user == null)
+                return HttpNotFound();
+            return View(new UsersEdit
+            {
+            Username = user.Username,
+            Email = user.Email
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int Id, UsersEdit form)
+        {
+            var user = Database.Session.Load<User>(Id);
+            if (user == null)
+                return HttpNotFound();
+            if (Database.Session.Query<User>().Any(w => w.Username == form.Username && w.Id != Id))
+                ModelState.AddModelError("Username", "Username must be unique");
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            user.Username = form.Username;
+            user.Email = form.Email;
+            Database.Session.Update(user);
+
+            return RedirectToAction("index");
+        }
+
+        public ActionResult ResetPassword(int Id)
+        {
+            var user = Database.Session.Load<User>(Id);
+            if (user == null)
+                return HttpNotFound();
+            return View(new UsersResetPassword
+            {
+                Username = user.Username,
+            });
+        }
+
+        [HttpPost]
+        public ActionResult ResetPassword(int Id, UsersResetPassword form)
+        {
+            var user = Database.Session.Load<User>(Id);
+            if (user == null)
+                return HttpNotFound();
+
+            form.Username = user.Username;
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            user.SetPassword(form.Password);
+            Database.Session.Update(user);
+
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            var user = Database.Session.Load<User>(Id);
+            if (user == null)
+                return HttpNotFound();
+
+            Database.Session.Delete(user);
+            return RedirectToAction("index");
+        }
     }
 }
--- SimpleBlog/Areas/Admin/ViewModels/Users.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Users.cs
@@ -1,9 +1,39 @@
 ﻿using System.Collections.Generic;
 using SimpleBlog.Models;
+using System.ComponentModel.DataAnnotations;
 namespace SimpleBlog.Areas.Admin.ViewModels
 {
     public class UsersIndex
     {
         public IEnumerable<User> Users { get; set; }
+    }
+
+    public class UsersNew
+    {
+        [Required,MaxLength(126)]
+        public string Username { get; set; }
+
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, MaxLength(256),DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+
+    public class UsersEdit
+    {
+        [Required, MaxLength(126)]
+        public string Username { get; set; }
+
+        [Required, MaxLength(256), DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+
+    public class UsersResetPassword
+    {
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
